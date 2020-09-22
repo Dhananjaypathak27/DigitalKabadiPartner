9454492bef09a2dca74f24a327d36af22b9be24a
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-09-22 23:14:22 +0530

    ProfileFragment added with profile icon

--- app/src/main/java/in/xparticle/digitalkabadipartner/MainActivity.java
+++ app/src/main/java/in/xparticle/digitalkabadipartner/MainActivity.java
@@ -7,11 +7,15 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView profilePhoto;
     SwipeRefreshLayout swipeRefreshLayout;
     BottomNavigationView bottomNav;
     @Override
@@ -21,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
         bottomNav = findViewById(R.id.bottomNavigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
-//        intent_function();
+        profilePhoto = findViewById(R.id.profile);
+//        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        intent_function();
 
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -31,25 +36,26 @@ public class MainActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-
+                intent_function();
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
+
+        profilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+            }
+        });
     }
-//    public void intent_function(){
-//
-//
-//    }
+    public void intent_function(){
 
-
-
-
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-        new BottomNavigationView.OnNavigationItemSelectedListener() {
+        Toast.makeText(MainActivity.this,"refresh Successful",Toast.LENGTH_SHORT).show();
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
                 Fragment selectedFragment = null;
 
                 switch (item.getItemId()){
@@ -63,7 +69,35 @@ public class MainActivity extends AppCompatActivity {
                 assert selectedFragment != null;
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         selectedFragment).commit();
+
                 return true;
             }
-        };
+        });
+    }
+
+
+
+
+
+
+//    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+//        new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                Fragment selectedFragment = null;
+//
+//                switch (item.getItemId()){
+//                    case R.id.nav_home:
+//                        selectedFragment =new HomeFragment();
+//                        break;
+//                    case R.id.nav_scrape_price:
+//                        selectedFragment = new ScrapePriceFragment();
+//                        break;
+//                }
+//                assert selectedFragment != null;
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        selectedFragment).commit();
+//                return true;
+//            }
+//        };
 }
--- app/src/main/java/in/xparticle/digitalkabadipartner/ProfileFragment.java
+++ app/src/main/java/in/xparticle/digitalkabadipartner/ProfileFragment.java
@@ -17,12 +17,13 @@ import android.webkit.WebViewClient;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ScrapePriceFragment#newInstance} factory method to
+ * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ScrapePriceFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     WebView webView;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,7 +33,7 @@ public class ScrapePriceFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ScrapePriceFragment() {
+    public ProfileFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +43,11 @@ public class ScrapePriceFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ScrapePriceFragment.
+     * @return A new instance of fragment ProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ScrapePriceFragment newInstance(String param1, String param2) {
-        ScrapePriceFragment fragment = new ScrapePriceFragment();
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,17 +68,18 @@ public class ScrapePriceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scrape_price, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        webView = view.findViewById(R.id.scrapePriceWebView);
+        webView = view.findViewById(R.id.profileWebView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("https://digitalkabadi.in/client/#scrapPrice");
-        webView.setWebViewClient(new ScrapePriceFragment.myWebclient());
+        webView.loadUrl("https://digitalkabadi.in/pickupboy/profile.php");
+        webView.setWebViewClient(new ProfileFragment.myWebclient());
     }
     public class myWebclient extends WebViewClient {
         @Override
