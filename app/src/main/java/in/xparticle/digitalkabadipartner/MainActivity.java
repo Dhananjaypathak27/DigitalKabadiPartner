44e3dc5caf2b5a32e4704b67dc649842a439d8a3
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2020-09-22 20:36:29 +0530

    HomeFragment and ScrapePriceFragment added to MainActivity, setOnNavigationListener added

--- app/src/main/java/in/xparticle/digitalkabadipartner/MainActivity.java
+++ app/src/main/java/in/xparticle/digitalkabadipartner/MainActivity.java
@@ -1,14 +1,69 @@
 package in.xparticle.digitalkabadipartner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout swipeRefreshLayout;
+    BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        bottomNav = findViewById(R.id.bottomNavigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+//        intent_function();
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
+//    public void intent_function(){
+//
+//
+//    }
+
+
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+        new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment =new HomeFragment();
+                        break;
+                    case R.id.nav_scrape_price:
+                        selectedFragment = new ScrapePriceFragment();
+                        break;
+                }
+                assert selectedFragment != null;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+                return true;
+            }
+        };
 }
